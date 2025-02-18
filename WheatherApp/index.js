1e4f69ac9b66d50bdf0d4ebc396ff53bd66b5f7a
vanshu778 <vthesiya778@gmail.com>
2025-02-18 08:47:00 +0530

    weather 3 part data fetch

--- WheatherApp/index.js
+++ WheatherApp/index.js
@@ -6,6 +6,10 @@ let w_temperature = document.querySelector(".weather_temperature");
 let w_minTem = document.querySelector(".weather_min");
 let w_maxTem = document.querySelector(".weather_max");
 
+let w_feelsLike = document.querySelector(".weather_feelsLike");
+let w_humidity = document.querySelector(".weather_humidity");
+let w_wind = document.querySelector(".weather_wind");
+let w_pressure = document.querySelector(".weather_pressure");
 
 // to get the actual country name
 const getCountryName = (code) => {
@@ -49,7 +53,11 @@ const getWeatherData = async() => {
         w_minTem.innerHTML = `Min: ${main.temp_min.toFixed()}&#176`;
         w_maxTem.innerHTML = `Max: ${main.temp_max.toFixed()}&#176`;
 
-        
+        w_feelsLike.innerHTML = `${main.feels_like.toFixed(2)}&#176`;
+        w_humidity.innerHTML = `${main.humidity}%`;
+        w_wind.innerHTML = `${wind.speed} m/s`;
+        w_pressure.innerHTML = `${main.pressure} hPa`;
+    
     }catch(error){
         console.log(error);
     }
