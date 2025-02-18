ca06cc3b6fe9e8d2019b6c67d8187fef5b50f450
vanshu778 <vthesiya778@gmail.com>
2025-02-18 08:28:46 +0530

    step 2 fetch weather data

--- WheatherApp/index.js
+++ WheatherApp/index.js
@@ -5,3 +5,20 @@ let w_icon = document.querySelector(".weather_icon");
 let w_temperature = document.querySelector(".weather_temperature");
 let w_minTem = document.querySelector(".weather_min");
 let w_maxTem = document.querySelector(".weather_max");
+
+const getWeatherData = async() => {
+    const weatherUrl =`https://api.openweathermap.org/data/2.5/weather?q=pune&APPID=a5fd05c1f3d9ddfd7ad33293ea7e340e
+`;
+
+    try{
+        const res = await fetch(weatherUrl);
+        const data = await res.json();
+        // console.log(data);
+        
+        const {main, name, weather, wind, sys, dt} = data;
+    }catch(error){
+        console.log(error);
+    }
+};
+
+document.body.addEventListener("load",getWeatherData());
